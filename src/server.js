658287d94977eb8bd41f40e0b657ffd56d5b8a4c
TatianaPri <87344989+Strecoza@users.noreply.github.com>
2025-04-01 22:36:41 -0700

    db connection, user model+test, auth router+controller

--- src/server.js
+++ src/server.js
@@ -1,5 +1,21 @@
+require('dotenv').config();
+
 const { PORT = 8000 } = process.env;
 const app = require("./app");
+const connectDB = require('./db/db');
 
-const listener = () => console.log(`Listening on Port ${PORT}!`);
-app.listen(PORT, listener);
+const server = async () => {
+    try{
+        await connectDB();
+        app.listen(PORT, () => {
+            console.log(`Listening server on port ${PORT}!`);
+        });
+    } catch (err) {
+        console.log(`server failed:`, err.message);
+    }
+}
+
+server();
+
+//const listener = () => console.log(`Listening on Port ${PORT}!`);
+//app.listen(PORT, listener);
