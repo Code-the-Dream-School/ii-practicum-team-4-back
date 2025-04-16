a71c9affe2019e9392ce426a35120f0b13214dd6
TatianaPri <87344989+Strecoza@users.noreply.github.com>
2025-04-15 17:08:43 -0700

    MBA-26: Auth middlware and correct userModel.test.js

--- src/tests/userModel.test.js
+++ src/tests/userModel.test.js
@@ -4,6 +4,8 @@ const {MongoMemoryServer} = require ('mongodb-memory-server');
 
 let mongoServer;
 
+jest.setTimeout(10000);
+
 beforeAll (async () => {
     mongoServer = await MongoMemoryServer.create();
     const uri = mongoServer.getUri();
@@ -12,7 +14,11 @@ beforeAll (async () => {
 
 afterAll(async () => {
     await mongoose.disconnect();
-    await mongoServer.stop();
+    if (mongoServer) await mongoServer.stop();
+});
+
+beforeEach(async () => {
+    await User.deleteMany();
 })
 
 describe( 'User model test', () => {
@@ -31,6 +37,26 @@ describe( 'User model test', () => {
         expect(savedUser.email).toBe(userData.email);
         expect(savedUser.password).not.toBe(userData.password);
     } );
+
+    it ('should not dublicate emails', async () => {
+        const userData = {
+            name: 'User One',
+            email: 'duplicate@example.com',
+            password: 'pass123',
+        };
+        await User.create(userData);
+
+        let err;
+        try{
+            await User.create(userData);
+        } catch( error) {
+            err = error
+        }
+
+        expect(err).toBeDefined();
+        expect(err.code).toBe(11000);
+    });
+
     it('should create valid JWT token', async () => {
         process.env.JWT_SECRET = 'testsecret';
         process.env.JWT_LIFETIME = '1h';
@@ -44,7 +70,21 @@ describe( 'User model test', () => {
 
         const token = user.createJWT();
         expect (typeof token).toBe('string');
-        expect(token).toMatch(/^[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+$/);
-        
+        expect(token).toMatch(/^[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+$/);    
+    } )
+
+    it ('should correctly compare password', async () => {
+        const user = new User({
+            name: 'Compare user',
+            email: 'compare@test.com',
+            password: 'mypassword',
+        });
+
+        await user.save();
+
+        const isValid = await user.comparePassword('mypassword');
+        const isInvalid = await user.comparePassword('wrongpassword');
+        expect (isValid).toBe(true);
+        expect(isInvalid).toBe(false);
     } )
 })
