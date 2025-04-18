94721e9ba3f6a7aa56a94effa4985342913845e7
TatianaPri <87344989+Strecoza@users.noreply.github.com>
2025-04-17 22:42:25 -0700

    Correct error handler

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,4 +1,5 @@
 const {StatusCodes} =  require ('http-status-codes');
+const {CustomAPIError} = require('../errors')
 
 const errorHandlerMiddleware = (err, req, res, next) => {
     let customError = {
@@ -7,25 +8,25 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     };
 
 if(err.name === 'ValidationError') {
-    customError.msg = Object.values(err.errors).map((item) => item/message).join(', ');
-    customError.statusCode = 400;
+    customError.msg = Object.values(err.errors).map((item) => item.message).join(', ');
+    customError.statusCode = StatusCodes.BAD_REQUEST;
 };
 
 if(err.code && err.code === 11000){
-    customError.msg = `Dublicate value for ${Object.keys(err.keyValue)} field`;
-    customError.statusCode = 400;
+    customError.msg = `User with this ${Object.keys(err.keyValue)} exist, try another`;
+    customError.statusCode = StatusCodes.BAD_REQUEST;
   };
 
   if(err.name === 'CastError'){
-    customError.msg = `No item found with id: ${err.value}`
-    customError.statusCode = 404;
+    customError.msg = `User with this: ${err.value} not found`
+    customError.statusCode = StatusCodes.NOT_FOUND;
   };
 
   if (err instanceof CustomAPIError) {
       return res.status(err.statusCode).json({ msg: err.message })
     }
 
-  return res.status(customError.statusCode).json({ msg: customError.message});
+  return res.status(customError.statusCode).json({ msg: customError.msg});
 }
 
 module.exports = errorHandlerMiddleware;
--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -3,8 +3,14 @@ const router = express.Router();
 const {register, login, getCurrentUser} = require('../controllers/authController');
 const authUser = require ("../middleware/authMiddleware");
 
+router.get('/', (req, res) => {
+    res.json({
+        msg: 'Auth route exist'
+    })
+})
+
 router.post('/register', register);
 router.post('/login', login);
-router.get("/current", authUser, getCurrentUser);
+router.get('/current', authUser, getCurrentUser);
 
 module.exports = router;
