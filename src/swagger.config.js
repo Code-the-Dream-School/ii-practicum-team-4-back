d88d3fd057329158b7f1390b698145037b170f81
OGoncharenko <olyas.goncharenko@gmail.com>
2025-05-13 10:58:20 -0700

    MBA-50: Add Swagger documentation for orders and products APIs

---
+++ src/swagger.config.js
@@ -0,0 +1,21 @@
+const swaggerJSDoc = require('swagger-jsdoc');
+
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Farm2You API',
+      version: '1.0.0',
+      description: 'API documentation for the Farm2You application',
+    },
+    servers: [
+      {
+        url: 'http://localhost:8000',
+      },
+    ],
+  },
+  apis: ['src/routes/*.js'],
+};
+
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
+module.exports = swaggerSpec;
