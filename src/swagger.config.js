243866152310a89d8cdcc7a411f36d56c0c09b02
TatianaPri <87344989+Strecoza@users.noreply.github.com>
2025-05-16 13:11:22 -0700

    MBA-52: Added Swagger documentation for user authentication, user profile, and boxes. (#12)
    * MBA-50: Add Swagger documentation for orders and products APIs
    
    * Swagger setup for user auth, user profile, main and boxes
    ---------
    
    Co-authored-by: OGoncharenko <olyas.goncharenko@gmail.com>

--- src/swagger.config.js
+++ src/swagger.config.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const swaggerJSDoc = require('swagger-jsdoc');
 
-
 const swaggerOptions = {
   definition: {
     openapi: '3.0.0',
@@ -15,6 +14,92 @@ const swaggerOptions = {
         url: process.env.BASE_URL,
       },
     ],
+     components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+      schemas: {
+        Address: {
+          type: 'object',
+          required: ['first_name', 'phone', 'address'],
+          properties: {
+            first_name: {
+              type: 'string',
+              example: 'Eva',
+            },
+            last_name: {
+              type: 'string',
+              example: 'Done',
+            },
+            phone: {
+              type: 'string',
+              example: '+1234567890',
+            },
+            email: {
+              type: 'string',
+              format: 'email',
+              example: 'eva@example.com',
+            },
+            address: {
+              type: 'string',
+              example: '123 First St',
+            },
+            additional_info: {
+              type: 'string',
+              example: 'Ring the bell',
+            },
+          },
+        },
+        User: {
+          type: 'object',
+          required: ['name', 'email', 'password'],
+          properties: {
+            _id: {
+              type: 'string',
+              example: '682525a5a6e051f98c6c00d0',
+            },
+            name: {
+              type: 'string',
+              minLength: 3,
+              maxLength: 30,
+              example: 'Eva Done',
+            },
+            email: {
+              type: 'string',
+              format: 'email',
+              example: 'eva@example.com',
+            },
+            password: {
+              type: 'string',
+              minLength: 7,
+              example: 'password123',
+            },
+            createdAt: {
+              type: 'string',
+              format: 'date-time',
+            },
+            updatedAt: {
+              type: 'string',
+              format: 'date-time',
+            },
+          },
+        },
+          Box: {
+            type: 'object',
+            required: ['name', 'weight', 'price'],
+            properties: {
+              _id: { type: 'string', example: '66541b7c1133e5f2c0001234' },
+              name: { type: 'string', example: 'Large Box', maxLength: 100 },
+              weight: { type: 'number', example: 12 },
+              price: { type: 'number', example: 15 },
+          },
+        },
+      },
+    },
   },
   apis: ['src/routes/*.js'],
 };
